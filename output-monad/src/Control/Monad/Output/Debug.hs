d8649e435ca639f2ed6257d2c57384f1aa34f324
Marcellus Siegburg <marcellussiegburg@users.noreply.github.com>
2024-04-25 17:03:30 +0200

    add additional info to testTask (#9) (#11)
    * add additional info to testTask (#9)
    
     - close #9

--- output-monad/src/Control/Monad/Output/Debug.hs
+++ output-monad/src/Control/Monad/Output/Debug.hs
@@ -4,6 +4,19 @@
 
 module Control.Monad.Output.Debug where
 
+import qualified Data.Text                        as T (unpack)
+import qualified Text.PrettyPrint.HughesPJClass   as HughesPJ (
+  Pretty,
+  pPrint,
+  render,
+  )
+import qualified Text.PrettyPrint.Leijen.Text     as Leijen (
+  Pretty,
+  displayTStrict,
+  pretty,
+  renderPretty,
+  )
+
 import Control.Monad.Output (
   GenericReportT,
   LangM',
@@ -14,8 +27,9 @@ import Control.Monad.Output.Generic (
   runLangMReport,
   )
 
-import Control.Monad                    (void)
-import Data.Maybe                       (isJust)
+import Control.Monad                    (void, when)
+import Control.Monad.Extra              (whenJust)
+import Data.Maybe                       (isJust, isNothing)
 
 showDescription
   :: (m ~ GenericReportT Language (IO ()) IO)
@@ -27,25 +41,44 @@ showDescription language generate f = do
   inst <- generate
   run language (f inst)
 
+data Display a where
+  Manual :: (a -> String) -> Display a
+  AutoHughesPJ :: HughesPJ.Pretty a => Display a
+  AutoLeijen :: Leijen.Pretty a => Display a
+
+display :: Display a -> a -> String
+display (Manual f) = f
+display AutoHughesPJ = HughesPJ.render . HughesPJ.pPrint
+display AutoLeijen = T.unpack
+  . Leijen.displayTStrict
+  . Leijen.renderPretty 0.4 80
+  . Leijen.pretty
+
 testTask
   :: (m ~ GenericReportT Language (IO ()) IO, Show a, Show b, Show c, Show d)
-  => Language
+  => Maybe (Display a)
+  -> Language
   -> IO inst
   -> (inst -> LangM' m b)
   -> (inst -> a -> LangM' m c)
   -> (inst -> a -> LangM' m d)
   -> IO a
   -> IO ()
-testTask language generate f partial full getSubmission = do
+testTask pretty language generate f partial full getSubmission = do
   inst <- generate
   desc <- run language (f inst)
   print desc
   value <- getSubmission
   putStrLn "---- Input ----"
   print value
+  whenJust pretty $ \displayMethod -> do
+    putStrLn "---- Prettified Input ----"
+    putStrLn $ display displayMethod value
   putStrLn "---- Partial ----"
   partialRes <- run language (partial inst value)
   print partialRes
+  when (isNothing partialRes)
+    $ putStrLn "!!! The following would not be printed in Autotool !!!"
   putStrLn "---- Complete ----"
   completeRes <- run language (full inst value)
   print completeRes
